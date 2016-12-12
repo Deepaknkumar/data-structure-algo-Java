b2609557ddfd63cbf866dcf93099077549910e24
Deepak Kumar <Deepak.n.sonukumar@gmail.com>
2016-12-12 01:31:09 -0800

    added GameEntry class

---
+++ src/GameEntry.java
@@ -0,0 +1,24 @@
+/**
+ * Created by Deepak on 12/12/2016.
+ */
+public class GameEntry {
+    private String name;
+    private int score;
+
+    public GameEntry(String n,int s){
+        this.name = n;
+        this.score = s;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getScore(){
+        return this.score;
+    }
+
+    public String toString(){
+        return "(" + name + "," + score + ")";
+    }
+}
---
+++ src/Scoreboard.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Deepak on 12/12/2016.
+ */
+public class Scoreboard {
+    private int numEntries = 0;
+    private GameEntry[] board;
+
+}
